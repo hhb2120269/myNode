cbe92a0cfec44590ecabc32b2bbe4d0b48381ed2
hhb2120269 <hehongbo1515@icloud.com>
2016-09-18 18:33:16 +0800

    no message

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var fs = require("fs");
 
-// 创建 application/x-www-form-urlencoded 编码解析
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var bodyParser = require('body-parser');
+var multer  = require('multer');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
 
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
 
-app.post('/process_post', urlencodedParser, function (req, res) {
+app.post('/file_upload', function (req, res) {
 
-   // 输出 JSON 格式
-   response = {
-       first_name:req.body.first_name,
-       last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
+   console.log(req.files[0]);  // 上传的文件信息
+
+   var des_file = __dirname + "/" + req.files[0].originalname;
+   fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+         if( err ){
+              console.log( err );
+         }else{
+               response = {
+                   message:'File uploaded successfully', 
+                   filename:req.files[0].originalname
+              };
+          }
+          console.log( response );
+          res.end( JSON.stringify( response ) );
+       });
+   });
 })
 
 var server = app.listen(8081, function () {
