992ba92dbcc7a89ecfc857e24fb79fa61f7df7c1
hhb <hehongbo1515@icloud.com>
2017-01-23 16:23:41 +0800

    1111

---
+++ servers/fshyServer.js
@@ -0,0 +1,66 @@
+/**
+ * Created by 贺弘博 on 2016/9/23.
+ */
+var path = require("path");
+var express = require('express');
+var router = express.Router();
+var fs = require("fs");
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+//custom
+var dPath = path.resolve();
+var tool = require(dPath+'/public/tool.js');
+
+
+router.get('/fshy', function (req, res) {
+    res.sendFile( dPath + "/views/index.html" );
+});
+router.get('/fshy/', function (req, res) {
+    res.sendFile( dPath + "/views/index.html" );
+});
+router.get('/fshy/cars', function (req, res) {
+    res.sendFile( dPath + "/views/index.html" );
+});
+router.get('/fshy', function (req, res) {
+    res.sendFile( dPath + "/views/download.html" );
+});
+//router.get('/chat', function(req, res){
+//    res.sendFile(dPath + "/views/chat.html");
+//});
+
+// 上传
+router.post('/file_upload', function (req, res) {
+
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = dPath + "/data/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if( err ){
+                console.log( err );
+            }else{
+                response = {
+                    message:'File uploaded successfully',
+                    filename:req.files[0].originalname
+                };
+            }
+            console.log( response );
+            res.end( JSON.stringify( response ) );
+        });
+    });
+});
+
+// 下载
+router.post('/file_download', function (req, res,next) {
+    console.log("start download");
+    var des_file = dPath + "/data/" + "gtslApp_signed.apk";
+    res.download(des_file);
+});
+
+
+//required
+module.exports = router;
+
